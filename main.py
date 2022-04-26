057a41079f88eecd764162641e4f2751bf42b383
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-04-25 21:46:47 -0400

    flipped forward

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ while running:
     turn = 0
 
     if driverController and driverController.get_init():
-        forward = driverController.get_axis(1)
+        forward = - driverController.get_axis(1)
         strafe = driverController.get_axis(0)
         turn = driverController.get_axis(2)
         leftJoystick.set_position((strafe * 30, forward * 30))
@@ -81,7 +81,6 @@ while running:
         turn_in_place()
 
     # Draw a solid blue circle in the center
-    # pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
     pygame.draw.rect(screen, (200, 200, 200), (300, 300, 400, 400))
     allSpriteGroup.update()
     allSpriteGroup.draw(screen)
